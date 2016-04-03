049a2d7a87a782f9a448052ef523b9946d956c54
Bartosz Marcinkowski <bm371613@gmail.com>
2016-04-03 12:37:56 +0200

    parsing file note

--- src/raise.c
+++ src/raise.c
@@ -2,11 +2,18 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 
 #define ALIGN4(value) (((value) + 3) & (~3) )
 #define PT_ANY -1
+
+typedef struct {
+	long prstatus;
+	long file;
+	long tls;
+} notes_desc_offsets;
 
 void error(char *msg) {
 	fprintf(stderr, "%s\n", msg);
@@ -37,8 +44,8 @@ void verify_header(FILE *f) {
 	if (hdr.e_version != EV_CURRENT) error("Bad version");
 }
 
-void for_each_segment(FILE *f, Elf32_Word type,
-		void (*func)(FILE*, Elf32_Phdr*)) {
+void for_each_segment(FILE *f, Elf32_Word type, void *result,
+		void (*func)(FILE*, Elf32_Phdr*, void*)) {
 	int i;
 	Elf32_Ehdr hdr;
 
@@ -48,21 +55,12 @@ void for_each_segment(FILE *f, Elf32_Word type,
 		read_at(f, hdr.e_phoff + i * hdr.e_phentsize, &phdr,
 				sizeof(Elf32_Phdr), 1);
 		if (phdr.p_type == type || type == PT_ANY)
-			(*func)(f, &phdr);
+			(*func)(f, &phdr, result);
 	}
 }
 
-void verify_segment_type(FILE *f, Elf32_Phdr *phdr) {
-	switch (phdr->p_type) {
-	case PT_LOAD:
-	case PT_NOTE:
-		break;
-	default:
-		error("Unknown segment type");
-	}
-}
-
-void handle_load_segment(FILE *f, Elf32_Phdr *phdr) {
+void load_load_segment(FILE *f, Elf32_Phdr *phdr, void *result) {
+	// TODO
 	printf("Load segment\n");
 	printf("p_offset\t%d\n", phdr->p_offset);
 	printf("p_vaddr\t%d\n", phdr->p_vaddr);
@@ -73,9 +71,10 @@ void handle_load_segment(FILE *f, Elf32_Phdr *phdr) {
 	printf("\n");
 }
 
-void handle_notes_segment(FILE *f, Elf32_Phdr *phdr) {
+void gather_relevant_notes(FILE *f, Elf32_Phdr *phdr, void *result) {
 	int note_hdr[3]; /* note header: name size, desc size, type */
-	int desc_offset, offset;
+	long desc_offset, offset;
+	notes_desc_offsets *desc_offsets = result;
 
 	offset = phdr->p_offset;
 	while (offset < phdr->p_offset + phdr->p_filesz) {
@@ -94,31 +93,57 @@ void handle_notes_segment(FILE *f, Elf32_Phdr *phdr) {
 
 		switch (note_hdr[2]) {
 		case NT_PRSTATUS:
-		case NT_FPREGSET:
-		case NT_PRPSINFO:
-		case NT_AUXV:
-		case NT_SIGINFO:
-		case NT_FILE:
-		case NT_PRXFPREG:
-		case NT_386_TLS:
-		case NT_X86_XSTATE:
-			printf("Found known note: 0x%x\n", note_hdr[2]);
+			desc_offsets->prstatus = desc_offset;
 			break;
-		default:
-			error("Unknown note type");
+		case NT_FILE:
+			desc_offsets->file= desc_offset;
+			break;
+		case NT_386_TLS:
+			desc_offsets->tls = desc_offset;
+			break;
 		}
 	}
 }
 
+void map_files(FILE *f, long nt_file_desc_offset) {
+	unsigned hdr[2]; /* count, page size (unit for file offset) */
+	unsigned long entry[3]; /* start, end, file offset */
+	char fname[256];
+	long entry_offset, fname_offset;
+	int i;
+
+	read_at(f, nt_file_desc_offset, hdr, sizeof(hdr), 1);
+	entry_offset = nt_file_desc_offset + sizeof(hdr);
+	fname_offset = entry_offset + hdr[0] * sizeof(entry);
+
+	for (i = 0; i < hdr[0]; ++i) {
+		read_at(f, entry_offset, entry, sizeof(entry), 1);
+		read_at(f, fname_offset, fname, 1, sizeof(fname));
+		fname[255] = '\0';
+
+		printf("%10lx %10lx %10lu %s\n",
+				entry[0], entry[1], entry[2], fname);
+		// TODO
+
+		entry_offset += sizeof(entry);
+		fname_offset += strlen(fname) + 1;
+	}
+}
+
+
 int main(int argc, char *argv[]) {
 	FILE *f;
+	notes_desc_offsets nd_offsets;
 
 	if (argc != 2) error("Supply exactly one argument (core file name)");
 	f = fopen(argv[1], "r");
 	if (f == NULL) error("Failed to open the file");
+
 	verify_header(f);
-	for_each_segment(f, PT_ANY, verify_segment_type);
-	for_each_segment(f, PT_LOAD, handle_load_segment);
-	for_each_segment(f, PT_NOTE, handle_notes_segment);
+	for_each_segment(f, PT_NOTE, &nd_offsets, gather_relevant_notes);
+	for_each_segment(f, PT_LOAD, NULL, load_load_segment);
+
+	map_files(f, nd_offsets.file);
+
 	fclose(f);
 }

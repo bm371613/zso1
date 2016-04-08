b4cf8c01afa95d68c5465533377e593a08e95a22
Bartosz Marcinkowski <bm371613@gmail.com>
2016-04-08 21:10:45 +0200

    no stdio

--- src/raise.c
+++ src/raise.c
@@ -1,7 +1,6 @@
 #include <asm/ldt.h>
 #include <elf.h>
 #include <fcntl.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/mman.h>
@@ -11,6 +10,9 @@
 #define ALIGN4(value) (((value) + 3) & (~3) )
 #define FILENAME_SIZE 256
 #define NOTE_FILE_ENTRY_SIZE (3 * sizeof(unsigned long))
+
+int fd;
+Elf32_Ehdr hdr;
 
 /* context switching */
 char filename[FILENAME_SIZE];
@@ -41,60 +43,47 @@ void error(char *msg) {
 	exit(1);
 }
 
-int read_at(FILE *f, long offset, void *ptr, size_t size, size_t nmemb) {
-	fseek(f, offset, SEEK_SET);
-	return fread(ptr, size, nmemb, f);
-}
+int read_at(long offset, void *ptr, size_t size) {
+	int read_count;
 
-void read_header(FILE *f, Elf32_Ehdr *hdr) {
-	if (read_at(f, 0, hdr, sizeof(Elf32_Ehdr), 1) != 1)
-		error("Failed to read ELF header");
-}
-
-void verify_header(FILE *f) {
-	Elf32_Ehdr hdr;
-
-	read_header(f, &hdr);
-	if (hdr.e_ident[EI_MAG0] != ELFMAG0
-			|| hdr.e_ident[EI_MAG1] != ELFMAG1
-			|| hdr.e_ident[EI_MAG2] != ELFMAG2
-			|| hdr.e_ident[EI_MAG3] != ELFMAG3)
-		error("Not an ELF");
-	if (hdr.e_type != ET_CORE) error("Not a core ELF");
-	if (hdr.e_machine != EM_386) error("Bad machine");
-	if (hdr.e_version != EV_CURRENT) error("Bad version");
-}
-
-void for_each_segment(FILE *f, Elf32_Word type,
-		void (*func)(FILE*, Elf32_Phdr*)) {
-	int i;
-	Elf32_Ehdr hdr;
-
-	read_header(f, &hdr);
-	for (i = 0; i < hdr.e_phnum; ++i) {
-		Elf32_Phdr phdr;
-		read_at(f, hdr.e_phoff + i * hdr.e_phentsize, &phdr,
-				sizeof(Elf32_Phdr), 1);
-		if (phdr.p_type == type)
-			(*func)(f, &phdr);
+	if (lseek(fd, offset, SEEK_SET) < 0)
+		error("lseek");
+	while (size > 0) {
+		read_count = read(fd, ptr, size);
+		if (read_count < 0)
+			error("read");
+		ptr += read_count;
+		size -= read_count;
 	}
 }
 
-void process_note_file(FILE *f, long note_file_desc_offset) {
+void for_each_segment(Elf32_Word type, void (*func)(Elf32_Phdr*)) {
+	int i;
+
+	for (i = 0; i < hdr.e_phnum; ++i) {
+		Elf32_Phdr phdr;
+		read_at(hdr.e_phoff + i * hdr.e_phentsize, &phdr,
+				sizeof(Elf32_Phdr));
+		if (phdr.p_type == type)
+			(*func)(&phdr);
+	}
+}
+
+void process_note_file(long note_file_desc_offset) {
 	long entry_offset, filename_offset;
 	int i;
 
-	read_at(f, note_file_desc_offset, &note_file_header,
-			sizeof(note_file_header), 1);
+	read_at(note_file_desc_offset, &note_file_header,
+			sizeof(note_file_header));
 	entry_offset = note_file_desc_offset + sizeof(note_file_header);
 	filename_offset = entry_offset
 		+ note_file_header.count * NOTE_FILE_ENTRY_SIZE ;
 
 	for (i = 0; i < note_file_header.count; ++i) {
-		read_at(f, entry_offset, &note_file_files[i],
-				NOTE_FILE_ENTRY_SIZE, 1);
-		read_at(f, filename_offset, note_file_files[i].filename,
-				1, FILENAME_SIZE);
+		read_at(entry_offset, &note_file_files[i],
+				NOTE_FILE_ENTRY_SIZE);
+		read_at(filename_offset, note_file_files[i].filename,
+				FILENAME_SIZE);
 		note_file_files[i].filename[FILENAME_SIZE - 1] = '\0';
 
 		entry_offset += NOTE_FILE_ENTRY_SIZE;
@@ -102,7 +91,7 @@ void process_note_file(FILE *f, long note_file_desc_offset) {
 	}
 }
 
-void process_segment_note(FILE *f, Elf32_Phdr *phdr) {
+void process_segment_note(Elf32_Phdr *phdr) {
 	long desc_offset, offset;
 	struct {
 		int name_size;
@@ -112,7 +101,7 @@ void process_segment_note(FILE *f, Elf32_Phdr *phdr) {
 
 	offset = phdr->p_offset;
 	while (offset < phdr->p_offset + phdr->p_filesz) {
-		read_at(f, offset, &note_header, sizeof(note_header), 1);
+		read_at(offset, &note_header, sizeof(note_header));
 
 		// offset at current note
 		offset += sizeof(note_header);
@@ -127,42 +116,41 @@ void process_segment_note(FILE *f, Elf32_Phdr *phdr) {
 
 		switch (note_header.type) {
 		case NT_PRSTATUS:
-			read_at(f, desc_offset, &prstatus,
-				sizeof(prstatus), 1);
+			read_at(desc_offset, &prstatus, sizeof(prstatus));
 			break;
 		case NT_FILE:
-			process_note_file(f, desc_offset);
+			process_note_file(desc_offset);
 			break;
 		case NT_386_TLS:
-			read_at(f, desc_offset, &user_desc,
-				sizeof(user_desc), 1);
+			read_at(desc_offset, &user_desc, sizeof(user_desc));
 			break;
 		}
 	}
 }
 
-void process_segment_load(FILE *f, Elf32_Phdr *phdr) {
-	int i, fd, prot;
-	struct note_file_file *file = NULL;
+void process_segment_load(Elf32_Phdr *phdr) {
+	int i, b_fd, prot;
+	struct note_file_file *b_file = NULL;
 	void *mem;
 
 	/* check if there is a  backing file */
 	for (i = 0; i < note_file_header.count; ++i)
 		if (phdr->p_vaddr == note_file_files[i].start)
-			file = &note_file_files[i];
+			b_file = &note_file_files[i];
 
 	/* map memory */
-	if (file == NULL) {
+	if (b_file == NULL) {
 		mem = mmap((void*) phdr->p_vaddr, phdr->p_memsz, PROT_WRITE,
 				MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
 	} else {
-		fd = open(file->filename, O_RDONLY);
-		if (fd == -1)
+		b_fd = open(b_file->filename, O_RDONLY);
+		if (b_fd == -1)
 			error("open mapped file");
 		mem = mmap((void*) phdr->p_vaddr, phdr->p_memsz, PROT_WRITE,
 				MAP_PRIVATE,
-				fd, file->pgoff * note_file_header.page_size);
-		close(fd);
+				b_fd,
+				b_file->pgoff * note_file_header.page_size);
+		close(b_fd);
 	}
 
 	if (mem == MAP_FAILED)
@@ -174,7 +162,7 @@ void process_segment_load(FILE *f, Elf32_Phdr *phdr) {
 		error("failed to map at requested address");
 
 	/* copy data */
-	read_at(f, phdr->p_offset, mem, 1, phdr->p_filesz);
+	read_at(phdr->p_offset, mem, phdr->p_filesz);
 
 	/* protect memory */
 	prot = PROT_NONE;
@@ -185,33 +173,37 @@ void process_segment_load(FILE *f, Elf32_Phdr *phdr) {
 		error("mprotect");
 }
 
-void unmap_old_stack() {
-	// TODO
-}
-
 void do_raise() {
-	FILE *f;
-
-	unmap_old_stack();
-
 	/* open file */
-	f = fopen(filename, "r");
-	if (f == NULL) error("Failed to open the file");
+	fd = open(filename, O_RDONLY);
+	if (fd < 0) error("Failed to open the file");
 
-	/* verify file, extract notes */
-	verify_header(f);
-	for_each_segment(f, PT_NOTE, process_segment_note);
+	/* read and verify header */
+	if (read_at(0, &hdr, sizeof(Elf32_Ehdr)) != sizeof(Elf32_Ehdr))
+		error("Failed to read ELF header");
+	if (hdr.e_ident[EI_MAG0] != ELFMAG0
+			|| hdr.e_ident[EI_MAG1] != ELFMAG1
+			|| hdr.e_ident[EI_MAG2] != ELFMAG2
+			|| hdr.e_ident[EI_MAG3] != ELFMAG3)
+		error("Not an ELF");
+	if (hdr.e_type != ET_CORE) error("Not a core ELF");
+	if (hdr.e_machine != EM_386) error("Bad machine");
+	if (hdr.e_version != EV_CURRENT) error("Bad version");
+
+	/* extract relvent info from notes */
+	for_each_segment(PT_NOTE, process_segment_note);
 
 	/* load segments */
-	for_each_segment(f, PT_LOAD, process_segment_load);
+	for_each_segment(PT_LOAD, process_segment_load);
 
 	/* close file */
-	fclose(f);
+	close(fd);
 
 	// TODO
 
 	/* tls */
 	/*printf("TLS %u: 0x%x %d\n\n", user_desc.entry_number,*/
+
 		/*user_desc.base_addr, user_desc.limit);*/
 
 	/* prstatus (see user_regs struct) */
